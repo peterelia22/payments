4f801bd3cf29d3c27b2ba2252514528314b42d22
peterelia22 <peterelia914@gmail.com>
2025-09-13 22:20:33 +0300

    Refactor code structure for improved readability and maintainability

--- lib/features/checkout features/presentation/views/widgets/payment_method_item.dart
+++ lib/features/checkout features/presentation/views/widgets/payment_method_item.dart
@@ -22,8 +22,9 @@ class PaymentMethodItem extends StatelessWidget {
         decoration: ShapeDecoration(
           shape: RoundedRectangleBorder(
             side: BorderSide(
-                width: 1.50,
-                color: isActive ? const Color(0xFF34A853) : Colors.grey),
+              width: 1.50,
+              color: isActive ? const Color(0xFF34A853) : Colors.grey,
+            ),
             borderRadius: BorderRadius.circular(15),
           ),
           shadows: [
@@ -32,18 +33,18 @@ class PaymentMethodItem extends StatelessWidget {
               blurRadius: 4,
               offset: const Offset(0, 0),
               spreadRadius: 0,
-            )
+            ),
           ],
         ),
         child: Container(
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(12)),
-            child: Center(
-                child: SvgPicture.asset(
-              image,
-              height: 40,
-              fit: BoxFit.scaleDown,
-            ))),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Center(
+            child: SvgPicture.asset(image, height: 40, fit: BoxFit.scaleDown),
+          ),
+        ),
       ),
     );
   }
