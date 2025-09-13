b8e1ed581e39374d531a0895a2d95942e12acfcd
peterelia22 <peterelia914@gmail.com>
2025-09-13 22:42:36 +0300

    Add payment intent model classes with JSON serialization support

---
+++ lib/features/checkout features/data/models/payment_intent_input_model.dart
@@ -0,0 +1,9 @@
+class PaymentIntentInputModel {
+  final String amount;
+  final String currency;
+
+  PaymentIntentInputModel({required this.amount, required this.currency});
+  toJson() {
+    return {'amount': amount, 'currency': currency};
+  }
+}
