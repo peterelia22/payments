cc306ef09fd1bf2c9660c6ab3d508f3ff8a10350
peterelia22 <peterelia914@gmail.com>
2025-09-21 12:38:39 +0300

    Add flutter_inappwebview dependency and register plugin for web and Windows platforms; implement Amount and Details models for checkout features

--- lib/features/checkout features/presentation/views/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout features/presentation/views/widgets/custom_button_bloc_consumer.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payments/core/widgets/custom_button.dart';
 import 'package:payments/features/checkout%20features/presentation/views/thank_you.dart';
 
@@ -46,8 +47,71 @@ class CustomButtonBlocConsumer extends StatelessWidget {
               ).makePayment(paymentIntentInputModel: paymentIntentInputModel);
             } else if (selectedMethod == 1) {
               // PayPal
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('PayPal not implemented yet')),
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => PaypalCheckoutView(
+                    sandboxMode: true,
+                    clientId: "",
+                    secretKey: "",
+                    transactions: const [
+                      {
+                        "amount": {
+                          "total": "55",
+                          "currency": "USD",
+                          "details": {
+                            "subtotal": "40",
+                            "shipping": "20",
+                            "shipping_discount": 0,
+                          },
+                        },
+                        "description": "The payment transaction description.",
+                        // "payment_options": {
+                        //   "allowed_payment_method":
+                        //       "INSTANT_FUNDING_SOURCE"
+                        // },
+                        "item_list": {
+                          "items": [
+                            {
+                              "name": "Apple",
+                              "quantity": 4,
+                              "price": '5',
+                              "currency": "USD",
+                            },
+                            {
+                              "name": "Pineapple",
+                              "quantity": 5,
+                              "price": '10',
+                              "currency": "USD",
+                            },
+                          ],
+
+                          // shipping address is not required though
+                          //   "shipping_address": {
+                          //     "recipient_name": "tharwat",
+                          //     "line1": "Alexandria",
+                          //     "line2": "",
+                          //     "city": "Alexandria",
+                          //     "country_code": "EG",
+                          //     "postal_code": "21505",
+                          //     "phone": "+00000000",
+                          //     "state": "Alexandria"
+                          //  },
+                        },
+                      },
+                    ],
+                    note: "Contact us for any questions on your order.",
+                    onSuccess: (Map params) async {
+                      print("onSuccess: $params");
+                    },
+                    onError: (error) {
+                      print("onError: $error");
+                      Navigator.pop(context);
+                    },
+                    onCancel: () {
+                      print('cancelled:');
+                    },
+                  ),
+                ),
               );
             } else if (selectedMethod == 2) {
               // Apple Pay
